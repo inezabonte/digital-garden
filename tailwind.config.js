4ccfdec8b2f9f45136c30a368e4fc71ee7b92df4
inezabonte <inezabonte@gmail.com>
2021-09-26 20:59:10 +0200

    fixed tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	purge: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./layouts/**/*.{js,ts,jsx,tsx}",
+	],
 	darkMode: "class",
 	theme: {
 		extend: {
