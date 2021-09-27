b2757236ef0d608b3843fbcb41846a0c14478852
inezabonte <inezabonte@gmail.com>
2021-09-27 12:38:47 +0200

    added tools and learning

--- pages/bookmarks/learning.js
+++ pages/bookmarks/learning.js
@@ -2,23 +2,45 @@ import { NextSeo } from "next-seo";
 import Layout from "@/layouts/MdxLayout";
 import { parseMDXContent } from "lib/mdx";
 import { MDXRemote } from "next-mdx-remote";
+import { getAllLearningBookmarks } from "lib/api";
+import { HiExternalLink } from "react-icons/hi";
 
-export default function Learning({ mdxSource }) {
+export default function Learning({ mdxSource, bookmarks }) {
 	return (
 		<Layout>
 			<NextSeo title={"Learning 📚 | Digital Garden 🌱"} />
 			<article className="prose prose-blue">
 				<MDXRemote {...mdxSource} />
 			</article>
+			<div className="space-y-6 mt-4">
+				{bookmarks.map((item) => (
+					<div key={item.link}>
+						<h3 className="text-xl font-bold">{item.name}</h3>
+						<span className="text-gray-600 dark:text-gray-400">
+							{item.description}
+						</span>
+						<a
+							href={item.link}
+							className="flex items-center text-blue-600 underline"
+							target="_blank"
+						>
+							<span>Link</span>
+							<HiExternalLink />
+						</a>
+					</div>
+				))}
+			</div>
 		</Layout>
 	);
 }
 
 export async function getStaticProps() {
 	const pageData = await parseMDXContent("learning", "content/pages/bookmarks");
+	const bookmarks = await getAllLearningBookmarks();
 	return {
 		props: {
 			...pageData,
+			bookmarks,
 		},
 	};
 }
