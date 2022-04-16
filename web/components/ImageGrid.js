2824c13958570a4ecf4d736b07382c8a06dc2d27
inezabonte <inezabonte@gmail.com>
2022-04-16 19:47:46 -0400

    center images

--- web/components/ImageGrid.js
+++ web/components/ImageGrid.js
@@ -1,23 +1,18 @@
-import { motion } from 'framer-motion'
-
 export default function ImageGrid({ images, setSelectedImg }) {
 	return (
 		<div className="grid grid-cols-3 gap-4 lg:gap-8 my-6 mx-auto">
 			{images.map((image) => (
-				<motion.div
-					whileHover={{ scale: 1.1, opacity: 1 }}
+				<div
 					key={image._id}
 					className="h-0 py-[50%] px-0 relative overflow-hidden rounded opacity-80"
 					onClick={() => setSelectedImg(image.image)}
 				>
-					<motion.img
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
+					<img
 						src={image.image}
 						alt={image.caption}
-						className="min-w-full min-h-full absolute top-0 left-0 max-w-[150%]"
+						className="w-full h-full absolute top-0 left-0 object-cover max-w-[150%]"
 					/>
-				</motion.div>
+				</div>
 			))}
 		</div>
 	)
