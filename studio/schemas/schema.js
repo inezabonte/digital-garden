006053db7dd5be32f488312846f224d976efe35f
inezabonte <inezabonte@gmail.com>
2022-04-16 13:22:02 -0400

    add gallery to studio

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,14 +1,15 @@
-import createSchema from "part:@sanity/base/schema-creator";
+import createSchema from 'part:@sanity/base/schema-creator'
 
-import schemaTypes from "all:part:@sanity/base/schema-type";
+import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-import til from "./til";
-import tools from "./bookmarks/tools";
-import design from "./bookmarks/design";
-import learning from "./bookmarks/learning";
-import library from "./bookmarks/library";
+import til from './til'
+import tools from './bookmarks/tools'
+import design from './bookmarks/design'
+import learning from './bookmarks/learning'
+import library from './bookmarks/library'
+import gallery from './gallery'
 
 export default createSchema({
-	name: "default",
-	types: schemaTypes.concat([til, tools, design, learning, library]),
-});
+	name: 'default',
+	types: schemaTypes.concat([til, tools, design, learning, library, gallery]),
+})
