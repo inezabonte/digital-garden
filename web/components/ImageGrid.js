534665610ed21a9aac82ef74a436b78ab8016300
inezabonte <inezabonte@gmail.com>
2022-04-28 23:16:25 -0400

    use nextjs image component

--- web/components/ImageGrid.js
+++ web/components/ImageGrid.js
@@ -1,15 +1,18 @@
+import Image from 'next/image'
+
 export default function ImageGrid({ images, setSelectedImg }) {
 	return (
 		<div className="grid grid-cols-3 gap-4 lg:gap-8 my-6 mx-auto">
 			{images.map((image) => (
 				<div
 					key={image._id}
-					className="h-0 py-[50%] px-0 relative overflow-hidden rounded opacity-80"
+					className="h-0 py-[50%] px-0 relative overflow-hidden rounded"
 					onClick={() => setSelectedImg(image.image)}
 				>
-					<img
+					<Image
 						src={image.image}
 						alt={image.caption}
+						layout="fill"
 						className="w-full h-full absolute top-0 left-0 object-cover max-w-[150%]"
 					/>
 				</div>
